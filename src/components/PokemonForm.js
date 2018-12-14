d38d75c7f59113ebac2bda13048903281735668d
barbaradecares <barbaradecares@hotmail.com>
2018-12-14 14:46:16 -0600

    done

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,16 +1,27 @@
-import React from 'react'
-import { Form } from 'semantic-ui-react'
+import React from "react";
+import { Form } from "semantic-ui-react";
 
 class PokemonForm extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
-      name: '',
-      hp: '',
-      frontUrl: '',
-      backUrl: ''
-    }
+      name: "",
+      hp: "",
+      frontUrl: "",
+      backUrl: ""
+    };
+  }
+
+  handleSubmit = () => {
+    this.props.createPokemon(this.state);
+  };
+
+  updateState(criteria, input) {
+    this.setState(state => {
+      state[criteria] = input;
+      return state;
+    });
   }
 
   render() {
@@ -19,16 +30,40 @@ class PokemonForm extends React.Component {
         <h3>Add a Pokemon!</h3>
         <Form onSubmit={this.handleSubmit}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input
+              fluid
+              label="Name"
+              placeholder="Name"
+              name="name"
+              onChange={e => this.updateState("name", e.target.value)}
+            />
+            <Form.Input
+              fluid
+              label="hp"
+              placeholder="hp"
+              name="hp"
+              onChange={e => this.updateState("hp", e.target.value)}
+            />
+            <Form.Input
+              fluid
+              label="Front Image URL"
+              placeholder="url"
+              name="frontUrl"
+              onChange={e => this.updateState("frontUrl", e.target.value)}
+            />
+            <Form.Input
+              fluid
+              label="Back Image URL"
+              placeholder="url"
+              name="backUrl"
+              onChange={e => this.updateState("backUrl", e.target.value)}
+            />
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
       </div>
-    )
+    );
   }
 }
 
-export default PokemonForm
+export default PokemonForm;
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -1,23 +1,93 @@
-import React from 'react'
-import PokemonCollection from './PokemonCollection'
-import PokemonForm from './PokemonForm'
-import { Search } from 'semantic-ui-react'
-import _ from 'lodash'
+import React from "react";
+import PokemonCollection from "./PokemonCollection";
+import PokemonForm from "./PokemonForm";
+import { Search } from "semantic-ui-react";
+import _ from "lodash";
+import { stat } from "fs";
 
 class PokemonPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      allPokemons: [],
+      // filter: false,
+      input: ""
+    };
+  }
+
+  search = _.debounce(input => {
+    this.setState(state => {
+      state.input = input;
+      return state;
+    }),
+      500;
+  });
+
+  componentDidMount() {
+    fetch("http://localhost:3000/pokemon")
+      .then(response => {
+        return response.json();
+      })
+      .then(allPokemons => {
+        this.setState(state => {
+          state.allPokemons = allPokemons;
+          return state;
+        });
+      });
+  }
+
+  createPokemon = newPoki => {
+    const newPokiObject = {
+      name: newPoki.name,
+      stats: [{ name: "hp", value: newPoki.hp }],
+      sprites: { front: newPoki.frontUrl, back: newPoki.backUrl }
+    };
+
+    fetch("http://localhost:3000/pokemon", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newPokiObject)
+    }).then(() => {
+      this.setState(
+        state => {
+          state.allPokemons = [newPokiObject, ...state.allPokemons];
+          return state;
+        },
+        () => {
+          this.pokemons();
+        }
+      );
+    });
+  };
+
+  pokemons() {
+    let pokemons = this.state.allPokemons;
+
+    pokemons = pokemons.filter(pokemon =>
+      pokemon.name.includes(this.state.input)
+    );
+
+    return pokemons;
+  }
+
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search
+          onSearchChange={e => this.search(e.target.value)}
+          showNoResults={false}
+        />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={this.pokemons()} />
         <br />
-        <PokemonForm />
+        <PokemonForm createPokemon={this.createPokemon} />
       </div>
-    )
+    );
   }
 }
 
-export default PokemonPage
+export default PokemonPage;
